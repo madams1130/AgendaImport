da96f4a6f5c67c0279932561aa52f08a0c853d81
Mason <madams3@swarthmore.edu>
2026-04-29 14:38:03 -0400

    Speaker Conundrum Handled

--- import_agenda.py
+++ import_agenda.py
@@ -6,10 +6,18 @@ import sys
 
 def clean(val):
     return str(val).replace("'", "''")
+try:
+    bigTable = pd.read_excel(sys.argv[1], engine="xlrd", skiprows=14)
+except IndexError:
+    print("Input Structure: ./import_agenda.py agenda.xls <table.xls>")
+    exit(1)
+except: 
+    print("Unrecognized Table.")
+    exit(1)
 
-bigTable = pd.read_excel(sys.argv[1], engine="xlrd", skiprows=14)
+# fun fact: I learned SQL in Paris, and had to google how some concepts translated to English!
 
-parsedAgenda = db_table("Agenda", {
+event_info = db_table("agenda", {
     "id": "integer PRIMARY KEY AUTOINCREMENT",
     "date": "text",
     "time_start": "text",
@@ -17,12 +25,21 @@ parsedAgenda = db_table("Agenda", {
     "session_type": "text",
     "title": "text",
     "location": "text",
-    "description": "text",
-    "speaker": "text"
+    "description": "text"
+})
+
+speakersTable = db_table("speakers", {
+    "id": "integer PRIMARY KEY AUTOINCREMENT",
+    "name": "text UNIQUE"
+})
+
+eventSpeakersTable = db_table("event_speakers", {
+    "event_id": "integer",
+    "speaker_id": "integer",
+    "PRIMARY KEY": "(event_id, speaker_id)"
 })
 
 colNames = list(bigTable.columns)
-trickyCols = colNames[0:4]
 
 bigTable = bigTable[bigTable[str(colNames[0])] != str(colNames[0])]
 bigTable = bigTable[bigTable[str(colNames[1])] != str(colNames[1])]
@@ -30,21 +47,58 @@ bigTable = bigTable[bigTable[str(colNames[2])] != str(colNames[2])]
 bigTable = bigTable[bigTable[str(colNames[3])] != str(colNames[3])]
 bigTable = bigTable[bigTable[str(colNames[4])] != str(colNames[4])]
 
+speaker_list = []
+
+# dictionary! avoid selecting while writing (SQLite doesn't like that)
+speaker_cache = {}
+
 for _, row in bigTable.iterrows():
-    parsedAgenda.insert({
+    event_id = event_info.insert({
         "date": clean(row[str(colNames[0])]),
         "time_start": clean(row[str(colNames[1])]),
         "time_end": clean(row[str(colNames[2])]),
         "session_type": clean(row[str(colNames[3])]),
         "title": clean(row[str(colNames[4])]),
         "location": clean(row[str(colNames[5])]),
-        "description": clean(row[str(colNames[6])]),
-        "speaker": clean(row[str(colNames[7])])
+        "description": clean(row[str(colNames[6])])
     })
 
-rows = parsedAgenda.select(columns=["id", "date", "time_start", "time_end", "session_type"])
+    # get raw speakers string
+    speakerRepository = clean(row["Speakers"])
 
-for row in rows:
-    print(row)
+    # turn into "mini-array" w/ semicolon delimiters
+    if not (speakerRepository == 'nan'):
+        speaker_list = ([s.strip() for s in speakerRepository.split(";") if s.strip()])
+    else:
+        speaker_list = []
+
+    for speaker in speaker_list:
+
+        # new and improved unicity enforcer
+        if speaker not in speaker_cache:
+            speakersTable.insert({"name": speaker})
+
+            result = speakersTable.select(
+                columns=["id"],
+                where={"name": speaker}
+            )
+
+            if result:
+                speaker_cache[speaker] = result[0]["id"]
+
+        speaker_id = speaker_cache.get(speaker)
+
+        if speaker_id:
+            eventSpeakersTable.insert({
+                "event_id": event_id,
+                "speaker_id": speaker_id
+            })
+
+#rows = speakersTable.select(columns=["id", "name"])
+
+#print(speaker_list)
+
+#for row in rows:
+#    print(row)
 
 #print(colNames)
--- lookup_agenda.py
+++ lookup_agenda.py
@@ -3,3 +3,57 @@
 from db_table import db_table 
 import pandas as pd
 import sys
+
+if (len(sys.argv) < 3):
+    print("Input format: ./lookup_agenda.py <column> <value>")
+    exit(1)
+# input: ./lookup_agenda.py <column> <value>
+
+event_info = db_table("agenda", {
+    "id": "integer PRIMARY KEY AUTOINCREMENT",
+    "date": "text",
+    "time_start": "text",
+    "time_end": "text",
+    "session_type": "text",
+    "title": "text",
+    "location": "text",
+    "description": "text"
+})
+
+speakersTable = db_table("speakers", {
+    "id": "integer PRIMARY KEY AUTOINCREMENT",
+    "name": "text UNIQUE"
+})
+
+eventSpeakersTable = db_table("event_speakers", {
+    "event_id": "integer",
+    "speaker_id": "integer",
+    "PRIMARY KEY": "(event_id, speaker_id)"
+})
+
+if (sys.argv[1] == "speaker"):
+    # if we're looking for a speaker, find all of their events!!
+
+    # get the id of our speaker
+    speakFind = (speakersTable.select(["id"], {"name" : sys.argv[2]}))[0]["id"]
+
+    # pull out all matching events!
+    searchResults = eventSpeakersTable.select(["event_id"], {"speaker_id" : speakFind})
+
+    for result in searchResults:
+        findnar = event_info.select([], {"id" : result["event_id"]})
+        print(findnar)
+
+else:
+    searchResults = event_info.select(["id"], {sys.argv[1] : sys.argv[2]})
+
+    for result in searchResults:
+        findnar = event_info.select([], {"id" : result["id"]})
+        print(findnar)
+
+#print(ids)
+
+#rows = event_info.select(columns=["id", "date", "time_start", "time_end", "session_type"])
+
+#for row in rows:
+#    print(row)
