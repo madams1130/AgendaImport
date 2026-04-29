d1fe213fa4efd72f9842ebe09e9d6b922d858d82
Mason <madams3@swarthmore.edu>
2026-04-29 16:05:57 -0400

    Subsession and pretty-print done

--- import_agenda.py
+++ import_agenda.py
@@ -4,8 +4,11 @@ from db_table import db_table
 import pandas as pd
 import sys
 
+# The Irish can and will mess up your SQL databases
 def clean(val):
     return str(val).replace("'", "''")
+
+# try to read in the table.
 try:
     bigTable = pd.read_excel(sys.argv[1], engine="xlrd", skiprows=14)
 except IndexError:
@@ -17,6 +20,7 @@ except:
 
 # fun fact: I learned SQL in Paris, and had to google how some concepts translated to English!
 
+# Main table: hold almost all info about events themselves.
 event_info = db_table("agenda", {
     "id": "integer PRIMARY KEY AUTOINCREMENT",
     "date": "text",
@@ -28,19 +32,30 @@ event_info = db_table("agenda", {
     "description": "text"
 })
 
+# Speakers Table: Who are these speakers?
 speakersTable = db_table("speakers", {
     "id": "integer PRIMARY KEY AUTOINCREMENT",
     "name": "text UNIQUE"
 })
 
+# Join table: Where have the speakers spoken?
 eventSpeakersTable = db_table("event_speakers", {
     "event_id": "integer",
     "speaker_id": "integer",
     "PRIMARY KEY": "(event_id, speaker_id)"
 })
 
+# Join table: what sessions are subsessions associated with?
+subsessionTable = db_table("sub_sessions", {
+    "main_id": "integer",
+    "subsession_id": "integer",
+    "PRIMARY KEY": "(main_id, subsession_id)"
+})
+
 colNames = list(bigTable.columns)
 
+# This is an aritfact of .xls I believe? Column names are present in the raw data,
+# many times over. We don't want those rows!
 bigTable = bigTable[bigTable[str(colNames[0])] != str(colNames[0])]
 bigTable = bigTable[bigTable[str(colNames[1])] != str(colNames[1])]
 bigTable = bigTable[bigTable[str(colNames[2])] != str(colNames[2])]
@@ -48,25 +63,49 @@ bigTable = bigTable[bigTable[str(colNames[3])] != str(colNames[3])]
 bigTable = bigTable[bigTable[str(colNames[4])] != str(colNames[4])]
 
 speaker_list = []
+order = 1
 
-# dictionary! avoid selecting while writing (SQLite doesn't like that)
+# maintain so that subsessions are coherent (first session will never be subsession)
+main_title = ""
+supersession_id = -1
+
+# dictionary! avoid selecting while writing
 speaker_cache = {}
 
 for _, row in bigTable.iterrows():
-    event_id = event_info.insert({
-        "date": clean(row[str(colNames[0])]),
-        "time_start": clean(row[str(colNames[1])]),
-        "time_end": clean(row[str(colNames[2])]),
-        "session_type": clean(row[str(colNames[3])]),
-        "title": clean(row[str(colNames[4])]),
-        "location": clean(row[str(colNames[5])]),
-        "description": clean(row[str(colNames[6])])
-    })
+
+    # if this is a main session, prepare for possible subsessions
+    if (clean(row[str(colNames[3])]) == "Session"):
+        main_title = clean(row[str(colNames[4])])
+        supersession_id = order
+        event_id = event_info.insert({
+            "date": clean(row[str(colNames[0])]),
+            "time_start": clean(row[str(colNames[1])]),
+            "time_end": clean(row[str(colNames[2])]),
+            "session_type": clean(row[str(colNames[3])]),
+            "title": clean(row[str(colNames[4])]),
+            "location": clean(row[str(colNames[5])]),
+            "description": clean(row[str(colNames[6])])
+        })
+    elif (clean(row[str(colNames[3])]) == "Sub"):
+        subsessionTable.insert({"main_id" : supersession_id, "subsession_id" : order})
+        event_id = event_info.insert({
+            "date": clean(row[str(colNames[0])]),
+            "time_start": clean(row[str(colNames[1])]),
+            "time_end": clean(row[str(colNames[2])]),
+            "session_type": ("Subsession of " + main_title),            #clean(row[str(colNames[3])]),
+            "title": clean(row[str(colNames[4])]),
+            "location": clean(row[str(colNames[5])]),
+            "description": clean(row[str(colNames[6])])
+        })
+    else:
+        print("Unrecognized Session Type.")
+        exit(1)
 
     # get raw speakers string
     speakerRepository = clean(row["Speakers"])
 
-    # turn into "mini-array" w/ semicolon delimiters
+    # turn into "mini-array" w/ semicolon delimiters (if it exists)
     if not (speakerRepository == 'nan'):
         speaker_list = ([s.strip() for s in speakerRepository.split(";") if s.strip()])
     else:
@@ -93,12 +132,5 @@ for _, row in bigTable.iterrows():
                 "event_id": event_id,
                 "speaker_id": speaker_id
             })
-
-#rows = speakersTable.select(columns=["id", "name"])
-
-#print(speaker_list)
-
-#for row in rows:
-#    print(row)
-
-#print(colNames)
+    # id isn't set before inserting, so track it with an accumulator.
+    order += 1
--- lookup_agenda.py
+++ lookup_agenda.py
@@ -3,11 +3,109 @@
 from db_table import db_table 
 import pandas as pd
 import sys
+import textwrap
+
+printed = []
+
+COL_WIDTHS = {
+    "title": 15,
+    "location": 15,
+    "session_type": 20
+}
+
+# store keys to loop over later
+EVENT_COLUMNS = [
+    #"id",
+    "date",
+    "time_start",
+    "time_end",
+    "session_type",
+    "title",
+    "location"
+]
+
+# Printing "nan" over and over offends my aesthetic sensibilities
+def clean(value):
+    if value is None:
+        return ""
+    if isinstance(value, float) and math.isnan(value):
+        return ""
+    if str(value).lower() == "nan":
+        return ""
+    return value
+
+# "re-couple" speaker table with event table in output
+def get_speakers(event_id):
+    speaker_links = eventSpeakersTable.select(["speaker_id"], {"event_id": event_id})
+    names = []
+
+    # get all speakers associated with an event!
+    # though this was originally in the agenda.xls file, my table structure 
+    # handles them through a join table, so you can't grab the speaker list directly.
+    # (plus, commas look better than semicolons!)
+    for link in speaker_links:
+        speaker = speakersTable.select(["name"], {"id": link["speaker_id"]})
+        if speaker:
+            names.append(speaker[0]["name"])
+
+    return names
+
+# because why write a bunch of nice code if your output is incomprehensible?
+def format_row(session):
+    for col in EVENT_COLUMNS:
+        # remove nans
+        val = clean(session.get(col, ""))
+        print(f"{col:<12}: {val}")
+
+    speakers = get_speakers(session["id"])
+    speakers = [clean(s) for s in speakers if clean(s)]
+
+    print(f"{'speakers':<12}: {', '.join(speakers) if speakers else ''}")
+
+    print()
+
+    desc = clean(session.get("description", ""))
+    if desc:
+        wrapped_desc = textwrap.fill(
+            desc,
+            width=80,
+            initial_indent="    ",
+            subsequent_indent="    "
+        )
+        print(wrapped_desc)
+
+    print("\n" + "-" * 80 + "\n")
+
+# what ties it all together
+def pretty_print_session(session_id):
+
+    # because subsessions can be printed independently, avoid dupes!
+    if session_id in printed:
+        return
+    printed.append(session_id)
+
+    rows = event_info.select(
+        [],
+        {"id": session_id}
+    )
+
+    if not rows:
+        return
+
+    session = rows[0]
+    format_row(session)
+    
+    # find all related subsessions for given session
+    subs = subsessionTable.select(["subsession_id"], {"main_id": session_id})
+
+    for sub in subs:
+        sub_id = sub["subsession_id"]
+        #no risk of infinite recursion, subsessions can't have sub-subsessions
+        pretty_print_session(sub_id)
 
 if (len(sys.argv) < 3):
     print("Input format: ./lookup_agenda.py <column> <value>")
     exit(1)
-# input: ./lookup_agenda.py <column> <value>
 
 event_info = db_table("agenda", {
     "id": "integer PRIMARY KEY AUTOINCREMENT",
@@ -31,29 +129,32 @@ eventSpeakersTable = db_table("event_speakers", {
     "PRIMARY KEY": "(event_id, speaker_id)"
 })
 
+subsessionTable = db_table("sub_sessions", {
+    "main_id": "integer",
+    "subsession_id": "integer",
+    "PRIMARY KEY": "(main_id, subsession_id)"
+})
+
 if (sys.argv[1] == "speaker"):
     # if we're looking for a speaker, find all of their events!!
 
     # get the id of our speaker
-    speakFind = (speakersTable.select(["id"], {"name" : sys.argv[2]}))[0]["id"]
+    try:
+        speakFind = (speakersTable.select(["id"], {"name" : sys.argv[2]}))[0]["id"]
+    except IndexError:
+        print("No matching speaker found.")
+        exit()
+
 
     # pull out all matching events!
     searchResults = eventSpeakersTable.select(["event_id"], {"speaker_id" : speakFind})
 
     for result in searchResults:
-        findnar = event_info.select([], {"id" : result["event_id"]})
-        print(findnar)
+        pretty_print_session(result["event_id"])
 
 else:
+    # normal search, less tricky
     searchResults = event_info.select(["id"], {sys.argv[1] : sys.argv[2]})
 
     for result in searchResults:
-        findnar = event_info.select([], {"id" : result["id"]})
-        print(findnar)
-
-#print(ids)
-
-#rows = event_info.select(columns=["id", "date", "time_start", "time_end", "session_type"])
-
-#for row in rows:
-#    print(row)
+        pretty_print_session(result["id"])
